ab3a38f6bfb4206a80b2d20947353fe986b5a424
Garrett Finucane <garrett@XC.home>
2014-12-31 10:31:32 -0500

    pulls comments and stuff

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"] 
-  [clj-http "0.7.7"]
+                  [clj-http "1.0.1"]
                    [cheshire "5.2.0"]
                   [com.soundcloud/java-api-wrapper "1.3.1"]
+                [ring/ring-servlet "1.2.0-RC1"]
   ]
-  :main sciencefair.core/main
-  )
+  :main sciencefair.core
+)
--- src/sciencefair/core.clj
+++ src/sciencefair/core.clj
@@ -1,14 +1,44 @@
 (ns sciencefair.core
-   (:require [sciencefair.soundcloud :refer :all]))
+   (:require [sciencefair.soundcloud :refer :all])
+   (:gen-class)
+   )
 
 (def settings {:client-id "86d37185f1e5dcdb022276e7f9801ac3" :client-secret "3089b64302a442d9667b2e63368541b3"})
 
-(defn main
+(defn get-attrs 
+ "Takes a list of maps and an attribute and returns a list of all those values"
+ [items attr]
+ (map #(% attr) items))
+
+(defn get-comments
+ "Takes a list or vector of ids and returns a map of ids to comments"
+ [ids]
+ (let [comments {}] 
+        (map #(assoc comments % (get-attrs (tracks settings {} % "comments") :body)) ids)
+  )
+)
+(defn analyze-sentiment
+ [a]
+ 1.0
+ )
+(defn sentize 
+ [comments]
+ (let [sentized {}]
+        (map #(assoc sentized (first %) (map analyze-sentiment (second %)))
+         sentized)
+ ))
+" For genre sentiment
+        download them all with sampling search take only ids using get-attrs
+        get-comments
+        run sentiment store values
+        ----
+        genre
+        followers"
+
+(defn -main
   "I don't do a whole lot."
-  []
-  (let [ x (tracks settings {"genres" "brostep"})]
-        (doseq [i x]
-        (println i)))
+  [& args]
+  ;(println (get-attrs (tracks settings {"genres" "brostep"}) :id))
+  (println (sentize (get-comments '(123504279 61278329 142285485))))
   ;(println (map #(% :body) (tracks settings (str 167444670) "comments")))
-  
 )
